ac45674c8ce9629ea1058f5896e7eeb9ad3effb2
Danny Sprouse <dannyssprouse@gmail.com>
2018-08-04 18:59:49 -0400

    first commit

---
+++ customer.js
@@ -0,0 +1,84 @@
+// NPM packages for project: mysql and inquirer to run program; cli table and colors for organizing/styling data
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var colors = require("colors");
+var Table = require("cli-table");
+
+// Makes SQL DB connection using credentials
+var connection = mysql.createConnection( {
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "monkeybrains",
+    database: "bamazon_db"
+});
+
+// Display list of available items in a table
+
+var displayProducts = function() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        var table = new Table ({
+            head: ['Item Id'.blue.bgYellow, 'Item Name'.blue.bgYellow, 'Department'.blue.bgYellow, 'Current Price'.blue.bgYellow, 'Quantity in Stock'.blue.bgYellow]
+        });
+
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log("Please select from the following inventory: ");
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+              
+        //Lists items in the table
+        console.log(table.toString());
+
+        // Asks user to select item ID and quantity from the returned items
+        inquirer.prompt([{
+            name: "item_id",
+            type: "input",
+            message: "Please enter the item ID number of the product you'd like to purchase.",
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "stock_quantity",
+            type: "input",
+            message: "How many of these items would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }]).then(function(answer) {
+
+            // This responds to the user input by checking inventory to respond to the order request
+            var chosenId = answer.item_id - 1
+            var chosenProduct = res[chosenId]
+            var chosenQuantity = answer.stock_quantity
+
+            if (chosenQuantity < res[chosenId].stock_quantity) {
+                console.log("Your total is: " + res[chosenId].price.toFixed(2) * chosenQuantity);
+                connection.query("UPDATE products SET ? WHERE ? item_id = ?", [{
+                    stock_quantity: res[chosenId].stock_quantity - chosenQuantity
+                }, {
+                   // Something missing here I can't figure out, it is not calculating the update products correctly
+                }], 
+                function(err, res) {
+                    console.log();
+                });
+
+            } else {
+                console.log("There is not enough inventory to complete your order.");
+            }
+        })
+    })
+}
+// Table loads and asks questions, but once order is complete cannot get table to refresh with reduced inventory
+
+displayProducts();
---
+++ inventory.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Dragon Age Inquisition", "Video Games", 39.95, 279);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Mass Effect", "Video Games", 22.95, 174);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Diablo III", "Video Games", 55.95, 122);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("L'Air du Desert Marocain by Tauer", "Men's Fragrance", 279.25, 164);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Ed Hardy Love & Luck for Men", "Men's Fragrance", 39.95, 315);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Invictus by Paco Rabanne", "Men's Fragrance", 39.95, 122);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Beats Studio3 Wireless Headphones - Blue", "Electronics", 39.95, 358);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("SVS SB-2000 Subwoofer 12-in, 500-Watts", "Electronics", 699.97, 295);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Belkin 8' 12-Outlet Power Strip Surge Protector", "Electronics", 19.29, 215);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Canon EOS 6D Mark II Digital SLR Camera", "Photography", 1597.00, 622);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Canon EF 16-35mm f/4L IS USM Lens", "Photography", 999.97, 324);
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ("Canon Battery Grip E20", "Photography", 279.95, 17);products
